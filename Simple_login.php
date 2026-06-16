9999aa48405fd9768f4d77e2d7805450d5f42e60
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-06-16 19:18:26 +0530

    Create Simple_login.php

---
+++ Simple_login.php
@@ -0,0 +1,13 @@
+<?php
+$username = "admin";
+$password = "123";
+
+if($username == "admin" && $password == "123")
+{
+    echo "Login Successful";
+}
+else
+{
+    echo "Invalid Username or Password";
+}
+?>
