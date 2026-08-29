a640a07c913ab8bd607de31a41dcdfb72c02b49f
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-08-29 15:15:53 +0530

    Create Student Grade Calculator.php

---
+++ Student Grade Calculator.php
@@ -0,0 +1,19 @@
+<?php
+$marks = 82;
+
+if ($marks >= 90)
+    $grade = "A+";
+elseif ($marks >= 80)
+    $grade = "A";
+elseif ($marks >= 70)
+    $grade = "B";
+elseif ($marks >= 60)
+    $grade = "C";
+elseif ($marks >= 50)
+    $grade = "D";
+else
+    $grade = "Fail";
+
+echo "Marks = $marks<br>";
+echo "Grade = $grade";
+?>
