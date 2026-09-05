758a099c1453eed0a5599e3fe791f2807510cce5
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-09-05 11:38:09 +0530

    Create PHP with HTML_Student_Details.php

---
+++ PHP with HTML_Student_Details.php
@@ -0,0 +1,16 @@
+<?php
+$name = "Chinmayi";
+$age = 22;
+$course = "MCA";
+
+echo "<html>";
+echo "<body>";
+
+echo "<h2>Student Details</h2>";
+echo "<p>Name: $name</p>";
+echo "<p>Age: $age</p>";
+echo "<p>Course: $course</p>";
+
+echo "</body>";
+echo "</html>";
+?>
