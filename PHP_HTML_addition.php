b6a1a5f81b36a7372978308da617f43e6b3dd2d7
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-09-05 11:37:41 +0530

    Create PHP_HTML_addition.php

---
+++ PHP_HTML_addition.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<h2>Add Two Numbers</h2>
+
+<?php
+$a = 20;
+$b = 30;
+$sum = $a + $b;
+?>
+
+<p>First Number: <?php echo $a; ?></p>
+<p>Second Number: <?php echo $b; ?></p>
+<p>Sum: <?php echo $sum; ?></p>
+
+</body>
+</html>
